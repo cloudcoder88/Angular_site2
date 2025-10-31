dde53a56436095db033374eaea1add875431fd3a
cloudcoder88 <btech4104@gmail.com>
2025-10-31 19:34:02 +0100

    i added the project and myservices components to routes,diversified the website,separated info into their components,removed the contents from the main index.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { MyServicesComponent } from './my-services/my-services.component';
 
 export const routes: Routes = [
 
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  {path:'myservices',component:MyServicesComponent},
   { path: '**', redirectTo: '' }
 ];
