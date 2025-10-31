c395472f3ecde76f0746f48f61b282f2901d68bf
cloudcoder88 <btech4104@gmail.com>
2025-10-31 20:27:15 +0100

    i added angular material imports to the app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
+
+
+//  Angular Material imports are imported in below
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimations(), MatSidenavModule,
+     MatToolbarModule, MatIconModule, MatListModule, MatButtonModule]
 };
