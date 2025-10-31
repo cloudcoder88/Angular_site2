8c7f3de7c1d61a54e2315e0dc9578aaf9194018f
cloudcoder88 <btech4104@gmail.com>
2025-10-31 20:08:59 +0100

    i installed angular material and added router module for routing in the app.component.ts,added router links for navigation in the html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {RouterModule} from '@angular/router'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone:true,
 })
 export class AppComponent {
   title = 'my new whip';
